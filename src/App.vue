8625b27942b59088441726fa1fd5a1acaf54f877
Ahmed <me@rancho.me>
2021-03-16 21:36:22 +0100

    refactor: Migrates into a vue-cli project setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <timer/>
+</template>
+
+<script>
+import Timer from "@/components/Timer";
+
+export default {
+  name: 'App',
+  components: {
+    Timer
+  }
+}
+</script>
+
+<style>
+#app {
+  background-color: bisque;
+}
+</style>
---
+++ src/components/Timer.vue
@@ -0,0 +1,69 @@
+<template>
+  <section class="section">
+
+  <div class="paper container">
+    <h2>Choose project:</h2>
+    <select v-model="selectedProject">
+      <option v-for="project in projects" :key="project.name" :value="project.id">
+        {{ project.name }}
+      </option>
+    </select>
+    <div class="selector-box"></div>
+    <div class="toggle">
+      <button v-on:click="projectStart">start</button>
+      <button v-on:click="projectStop">stop</button>
+    </div>
+    <span>{{statusMessage}}</span>
+    <br>
+    <span>{{statusMessageLine2}}</span>
+    <div class="status"></div>
+  </div>
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+let domain = 'http://localhost:7999/projects/'
+export default {
+  name: "Timer",
+  data () {
+    return {
+      projects: [],
+      selectedProject: null,
+      statusMessage: "Welcome. Please choose an action",
+      statusMessageLine2: ""
+    }
+  },
+  mounted () {
+    axios.get(domain)
+        .then(response => (this.projects = response.data))
+  },
+  methods: {
+    projectStart () {
+      let projectStartURL = domain + this.selectedProject + '/start'
+      axios.post(projectStartURL, {})
+          .then(response => {
+            this.statusMessage = "Project Started on " + response.data.start;
+            this.statusMessageLine2 = "timesheet ID: " + response.data.id;
+            console.log("starting log succeeded!");
+          })
+          .catch(function () {
+            this.statusMessage = "OOOpsi!!";
+            console.log("Faced an exception!")
+          })
+    },
+    projectStop () {
+      let projectStopURL = domain + this.selectedProject + '/stop'
+      axios.post(projectStopURL, {})
+          .then(response => {
+            this.statusMessage = "Project Stopped on " + response.data.end;
+            this.statusMessageLine2 = "timesheet ID: " + response.data.id;
+          })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
