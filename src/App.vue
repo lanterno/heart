6879987ef4a2884b4422dd49cc82c84533bb65c6
Ahmed <me@rancho.me>
2021-03-18 22:09:02 +0100

    feat: adds two counters: useless but playful

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <timer/>
+  <clicker startPoint=3 />
+  <clicker startPoint=7 />
 </template>
 
 <script>
 import Timer from "@/components/Timer";
+import clicker from "@/components/clicker";
 
 export default {
   name: 'App',
   components: {
-    Timer
+    Timer,
+    clicker,
   }
 }
 </script>
---
+++ src/components/clicker.vue
@@ -0,0 +1,25 @@
+<template>
+<button @click="counter++">{{counter}}</button>
+</template>
+
+<script>
+
+export default {
+  name: "clicker",
+  props: {
+    startPoint: {
+      type: Number,
+      default: 1
+    }
+  },
+  data () {
+    return {
+      counter: this.startPoint
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
